3afb07e49499c0faa43c097e4ef9fd59cbfb72e2
BrendenSeifried <brendenseifried@gmail.com>
2022-03-30 17:28:44 -0700

    got list to show up

---
+++ src/services/fetchutils.js
@@ -0,0 +1,6 @@
+import { checkError, client } from './client';
+
+export async function fetchToDo() {
+  const resp = await client.from('todos').select('*');
+  return checkError(resp);
+}
---
+++ src/views/Home/Home.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { fetchToDo } from '../../services/fetchutils';
+
+export default function Home() {
+  const [todo, setToDo] = useState([]);
+
+  useEffect(()=> {
+    const grabToDo = async () => {
+      const data = await fetchToDo();
+      setToDo(data);
+    };
+    grabToDo();
+  }, []);
+
+  return (
+    <div> Home
+      {todo.map ((data) =>(
+        <div key={data.id}>
+          <h1>{data.description}</h1>
+            
+        </div>
+      ))}
+    </div>
+  );
+}
